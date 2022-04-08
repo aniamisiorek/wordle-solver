17fd6ba9704def366b2b7543cf459c7043e07d0e
Ania Misiorek <misiorek.a@northeastern.edu>
2022-04-07 21:07:19 -0400

    working

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 from ucs import Search
 
-def return_name():
-    search = Search(('catch', [2, 2, 2, 2, 2]))
-    return search.uniformCostSearch()
+def return_word():
+    search = Search(('soapy', [2, 0, 1, 2, 0], ['adieu']))
+    return search.ucs_result()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(return_name())
+    print(return_word())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- ucs.py
+++ ucs.py
@@ -1,12 +1,12 @@
 # 5 letter word to be guessed
 # Green, yellow and gray as feedback ( 0 - green, 1 - yellow, 2 - grey )
 # We should keep track of letters and the color feedback received
-# Store preciously used letters with associated feedback and position
+# Store previously used letters with associated feedback and position
 
 # State represented by word + feedback + position for every letter in the word: [('a', 0, 2), ('i', 0, 2), ('r', 0, 2)]
 # We pass a tuple with (new state: [('b', 1, 0), ('a', 1, 1), ('r', 1, 2)], visited letters: [['t', 2, 0], ['x', 1, 1]], cost of word: 4)
-import queue
 import heapq
+
 
 def load_solutions():
     with open('wordle_solutions.txt') as solution_file:
@@ -15,107 +15,104 @@ def load_solutions():
 
 class Search:
 
+    # constructor: tuple with input[0] being a string that represents a guess
+    #                         input[1] being the feedback for this guess
+
+    # state: word descriptor(described below), set of used words, set of used letters
+    # word descriptor: for 'catch', [0 1 2 0 1] -> ('c', 0, 0), ('a', 1, 1), ('t', 2, 2), ('c', 0, 3), ('h', 1, 4)
     def __init__(self, word):
         self.solutions = set(load_solutions())
-        #print(self.solutions)
-        # For 5 letters
         # initial word is "catch" with everything greyed out
-        # self.initialState = ('c', 2, 0), ('a', 2, 1), ('t', 2, 2), ('c', 2, 3), ('h', 2, 4),
-        #                      set('catch'), set('c', 'a', 't', 'c', 'h')
-        self.initialState = self.create_word(word[0], word[1], set(), set())
-
-    """Goal test: Checks whether a word is valid based on the known conditions of the word"""
-    def valid_word(self, state):
-        counter = 0
-        for i in state:
-            if i[1] == 0:
-                counter += 1
-        if counter == len(state):
-            return True
-        return False
-
-    # checks to see if a possible solution matches with a word descriptor, green should exist in green and yellow should not be in the same spot
-    def check_word(self, possibleSolution, parentState, matches):
-        # checks if word has been used already
-        if parentState[1].contains(possibleSolution):
-            return False
-        # First obtain green matches from all matches (could be yellow or green)
-        green = []
-        for i in matches:
-            if i[2] == 0:
-                green.append(i)
-        green_letters = self.join_word(green)
-        # checks if letter in possible solution is already used in parent but not green and therefore must be discarded
-        for letter in possibleSolution:
-            if letter in self.join_word(parentState) and letter not in green_letters:
-                return False
-        # check if green is in correct spot
-        # Initialize a counter and look for matches
-        counter = 0
-        for letter in range(0, len(possibleSolution)):
-            if (possibleSolution[letter], 0, letter) in green:
-                counter += 1
-        # Return True if all greens are in the correct spot
-        if counter == len(green):
-            return True
+        # self.initialState = 'catch', ('c', 2, 0), ('a', 2, 1), ('t', 2, 2), ('c', 2, 3), ('h', 2, 4),
+        #                      set('catch'), set('c', 'a', 't', 'h')
+        self.initial_state = self.create_word(word[0], word[1], set(word[2]), set())
 
     # input will be a word followed by the feedback in the same format
-    def create_word(self, word, feedback, usedWords, usedLetters):
-        # word = 'catch'
-        # feedback = [2 2 2 2 2]
+    def create_word(self, word, feedback, used_words, used_letters):
         word_descriptor = []
-        usedWords.add(word)
+        # add the word to the used words
+        used_words.add(word)
         for position in range(0, len(word)):
             result = word[position], feedback[position], position
             word_descriptor.append(result)
+            # added grayed out letters to the used letters
             if feedback[position] == 2:
-                usedLetters.add(word[position])
-        return word_descriptor, usedWords, usedLetters
+                used_letters.add(word[position])
+        return word, word_descriptor, used_words, used_letters
 
-    """Determines the cost of a word, lower cost means this word is better. Words with higher costs are worse.
-    We should be picking a word with letters that are contained in as many solutions as possible. For each letter
-    shared with other solutions, +1"""
-    def word_cost(self):
-        return 1
+    # checks to see if a possible solution matches with a word descriptor, green should exist in green and yellow
+    # should not be in the same spot
+    def check_word(self, possible_solution, parent_state):
+        # checks if word has been used already
+        if parent_state[2].__contains__(possible_solution):
+            return None
+
+        # assume all feedback is grayed out
+        new_feedback = [2, 2, 2, 2, 2]
+
+        for i in range(0, len(possible_solution)):
+            letter = parent_state[1][i]
+            # green case
+            if letter[1] == 0 and possible_solution[i] != letter[0]:
+                return None
+            # need to readjust feedback
+            elif letter[1] == 0 and possible_solution[i] == letter[0]:
+                new_feedback[i] = 0
+
+            # yellow case
+            if letter[1] == 1 and possible_solution[i] == letter[0]:
+                return None
+            elif letter[1] == 1 and possible_solution[i] != letter[0]:
+                # seeing if the possible letter exists somewhere else in the possible word
+                flag = True
+                for j in range(0, len(possible_solution)):
+                    if letter[0] == possible_solution[j]:
+                        new_feedback[i] = 1
+                        flag = False
+                if flag:
+                    return None
+
+            # checking if any letter in possible solution has already been grayed out
+            if parent_state[3].__contains__(possible_solution[i]):
+                return None
+        new_state = self.create_word(possible_solution, new_feedback, parent_state[2], parent_state[3])
+        return new_state
 
     """ Returns a list of words from the solution set that match the given constraints.
     Match green letters, use yellow letters and avoid grey letters.
     """
-    def get_successor(self, parentState, matches):
+    def get_successor(self, parent_state):
         successors = []
-        for possibleSolution in self.solutions:
-            if self.check_word(possibleSolution, parentState, matches):
-                successors.append(possibleSolution)
+        for possible_solution in self.solutions:
+            new_state = self.check_word(possible_solution, parent_state)
+            if new_state is not None:
+                successors.append(new_state)
         return successors
 
-    """ Returns the word as a single string for a guess
-    """
-    def join_word(self, word_descriptor):
-        word = []
-        for i in word_descriptor:
-            word.append(i[0])
-        return ''.join(word)
+    def ucs_result(self):
+        path = self.uniform_cost_search()
+        return path[0]
 
-    def uniformCostSearch(self):
+    def uniform_cost_search(self):
         """Search the node of least total cost first."""
-        initial_state = self.initialState
+        initial_state = self.initial_state
         frontier = PriorityQueue()
-        frontier.push((initial_state, [], 0),1)
+        frontier.push((initial_state, [], 0), 0)
         explored = []
 
         while not frontier.isEmpty():
-            state, matches, totalCost = frontier.pop()
+            state, path, total_cost = frontier.pop()
             while state in explored:
-                state, matches, totalCost = frontier.pop()
+                state, path, total_cost = frontier.pop()
             explored.append(state)
-            if self.valid_word(state):
-                return self.join_word(state)
-            for neighbor in self.get_successor(state, matches):
-                nextCost = totalCost + 1 #neighbor[2]
-                nextNode = (neighbor, list(matches) + [neighbor[1]], nextCost)
-                if neighbor[0] not in explored:
-                    frontier.push(nextNode, nextCost)
-
+            successors = self.get_successor(state)
+            if len(successors) == 0:
+                return path
+            for new_word in successors:
+                next_cost = total_cost + 1
+                next_node = new_word, list(path) + [new_word[0]], next_cost
+                if new_word[0] not in explored:
+                    frontier.push(next_node, next_cost)
 
 
 class PriorityQueue:
@@ -125,7 +122,8 @@ class PriorityQueue:
       in quick retrieval of the lowest-priority item in the queue. This
       data structure allows O(1) access to the lowest-priority item.
     """
-    def  __init__(self):
+
+    def __init__(self):
         self.heap = []
         self.count = 0
 
